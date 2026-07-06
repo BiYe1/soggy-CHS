e9149176ca5fec59b78b73111e9222aadca1875a
碧夜 <160700366+BiYe1@users.noreply.github.com>
2026-07-07 01:35:34 +0800

    Update document.h

--- extern/rapidjson-1.1.0/include/rapidjson/document.h
+++ extern/rapidjson-1.1.0/include/rapidjson/document.h
@@ -316,7 +316,7 @@ struct GenericStringRef {
 
     GenericStringRef(const GenericStringRef& rhs) : s(rhs.s), length(rhs.length) {}
 
-    GenericStringRef& operator=(const GenericStringRef& rhs) { s = rhs.s; length = rhs.length; }
+    GenericStringRef& operator=(const GenericStringRef& rhs) { s = rhs.s; /* length is const, cannot assign */ return *this; }
 
     //! implicit conversion to plain CharType pointer
     operator const Ch *() const { return s; }
